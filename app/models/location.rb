49b59c4446d6246223d7aa80e4aff9ac3524df94
Salami, Tope <salami.tope@gmail.com>
2016-01-11 22:02:18 +0900

    introducing migrations

---
+++ app/models/location.rb
@@ -0,0 +1,16 @@
+class Location < ActiveRecord::Base
+
+	def to_s
+    "#{id} - Created: #{created_at} - Updated: #{updated_at}"
+
+    end
+    
+    def telephone=(value)
+    	write_attribute(:telephone, value.gsub(/[^0-9]/i, ''))
+    end
+
+    def address
+    	street + ' ' + city + ', ' + state + ' ' + zip
+    end
+
+end
